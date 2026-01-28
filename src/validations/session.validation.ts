5b4fc1c719be2e762673266d6611858121bf1cb5
Moran Liv <moran.liv2006@gmail.com>
2026-01-28 22:22:12 +0200

    Added a access token difference validation between the first and second login.

--- src/validations/session.validation.ts
+++ src/validations/session.validation.ts
@@ -4,6 +4,7 @@ import {Login} from "../models/login.model";
 export async function validateSessionsPersist(secondLoginResponse: Login, firstLoginResponse: Login) {
     // Since the first login created the account, the second login shouldn't create account again (user session persist)
     expect(secondLoginResponse.response.LoginResponse.AccountCreated).toBe(false)
+    expect(secondLoginResponse.response.LoginResponse.AccessToken).not.toBe(firstLoginResponse.response.LoginResponse.AccessToken)
 
     expect(secondLoginResponse.response.LoginResponse.Session.SessionCounter).toEqual(firstLoginResponse.response.LoginResponse.Session.SessionCounter)
     expect(secondLoginResponse.response.LoginResponse.Session.SessionStartTtlSec).toBeGreaterThan(firstLoginResponse.response.LoginResponse.Session.SessionStartTtlSec)
