e0d34d31c89834d19090d0eab7d6e7281d307bce
Moran Liv <moran.liv2006@gmail.com>
2026-01-28 20:57:07 +0200

    Added a comment for more clarity.

--- src/validations/session.validation.ts
+++ src/validations/session.validation.ts
@@ -2,7 +2,9 @@ import {expect} from "@playwright/test";
 import {Login} from "../models/login.model";
 
 export async function validateSessionsPersist(secondLoginResponse: Login, firstLoginResponse: Login) {
+    // Since the first login created the account, the second login shouldn't create account again (user session persist)
     expect(secondLoginResponse.response.LoginResponse.AccountCreated).toBe(false)
+
     expect(secondLoginResponse.response.LoginResponse.Session.SessionCounter).toEqual(firstLoginResponse.response.LoginResponse.Session.SessionCounter)
     expect(secondLoginResponse.response.LoginResponse.Session.SessionStartTtlSec).toBeGreaterThan(firstLoginResponse.response.LoginResponse.Session.SessionStartTtlSec)
 }
