4cbfaa84387b2adde7b1dee84b35b52fc19fbf8f
Moran Liv <moran.liv2006@gmail.com>
2026-01-28 18:45:39 +0200

    Add ErrorMessages enum for the negative scenarios.

---
+++ src/helpers/error-messages.ts
@@ -0,0 +1,5 @@
+export enum ErrorMessages {
+    NOT_ENOUGH_RESOURCES = "NotEnoughResources",
+    GENERAL_ACCESS_TOKEN_ISSUES = "GENERAL ACCESS TOKEN ISSUES. accessToken=",
+    LOGINSOURCE_NOT_RECEIVED = "loginSource not received"
+}
